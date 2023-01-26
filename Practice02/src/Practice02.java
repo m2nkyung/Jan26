95a54c36f515e1d2638882f54f5b87a44e04d060
jerrx18 <happyjerrychristmas@gmail.com>
2023-01-26 19:47:17 +0900

    commit

---
+++ Practice02/src/Practice02.java
@@ -0,0 +1,15 @@
+/* 실행  예제  (입력  없음)
+가로  10, 세로  20의  사각형의  넓이는  200이다
+
+•   변수  width와  height를  선언하고  값을  대입한다. (더  이상의  변수는  사용하지  말라)
+•   하나의  System.out.println을  이용하여  출력한다 */
+
+public class Practice02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int width = 10;
+		int height = 20;
+		
+		System.out.println("가로 " + width + ", 세로 " + height + "의 사각형의 넓이는 " + (width * height) + "이다");
+	}
+}
