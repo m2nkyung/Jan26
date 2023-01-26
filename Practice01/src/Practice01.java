cbc13d42c23fb5fbc4a026b33502a3ffc341c963
jerrx18 <happyjerrychristmas@gmail.com>
2023-01-26 19:46:49 +0900

    commit

---
+++ Practice01/src/Practice01.java
@@ -0,0 +1,13 @@
+/* 아래와  같은  실행을  하는  프로그램을  작성하라.
+실행  결과
+I love Dongduk,
+and I love Java Programming*/
+
+public class Practice01 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I love Dongduk,");
+		System.out.println("and I love Java Programming");		
+	}
+}
